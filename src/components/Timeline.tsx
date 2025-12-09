e6c0ac62dd352d9862a90bc640ee15a74eb7a849
Ömer <savas.qrx@gmail.com>
2025-12-09 14:58:52 +0100

    update font

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -52,16 +52,16 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
               </div>
               <div className="flex-col hidden gap-2 text-xl font-bold md:flex md:pl-20 md:text-4xl text-neutral-300">
                 <h3>{item.date}</h3>
-                <h3 className="text-3xl md:text-[20px] lg:text-2xl text-neutral-400">
+                <h3 className="text-xl md:text-[20px] md:text-2xl text-neutral-400 ">
                   {item.title}
                 </h3>
-                <h3 className="text-3xl md:text-[20px] lg:text-2xl text-neutral-500">
+                <h3 className="text-xl md:text-[20px] md:text-2xl text-neutral-500">
                   {item.job}
                 </h3>
               </div>
             </div>
 
-            <div className="relative w-full pl-20 pr-4 md:pl-4">
+            <div className="relative w-full pl-8 pr-4 md:pl-4">
               <div className="block mb-4 text-2xl font-bold text-left text-neutral-300 md:hidden ">
                 <h3>{item.date}</h3>
                 <h3>{item.job}</h3>
