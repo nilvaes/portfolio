286c50b0498de1b195a943daba4b062930d5ffa2
Ömer <savas.qrx@gmail.com>
2025-12-09 13:35:23 +0100

    updata config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,16 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/portfolio/",
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react(), tailwindcss()],
+    base: "/", // Standard für Localhost
+  };
+
+  // Nur wenn wir für Produktion bauen (npm run build), ändern wir die Base
+  if (command !== "serve") {
+    config.base = "/portfolio/";
+  }
+
+  return config;
 });
