9c872e90981ed0e2968b41c297eb1c9b08eacc43
Ömer <savas.qrx@gmail.com>
2025-12-13 13:42:05 +0100

    add social media icons

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,6 +1,23 @@
 export default function Footer() {
   return (
-    <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
+    <section className="flex flex-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space mt-10 lg:mt-20">
+      <div className="flex justify-center items-center gap-3">
+        <a
+          href="https://www.linkedin.com/in/%C3%B6mer-savas-b31643399/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="w-10"
+        >
+          <img className="w-8 md:w-10" src="assets/logos/linkedin-white.svg" />
+        </a>
+        <a
+          href="https://github.com/nilvaes"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="w-10 md:w-12" src="assets/logos/github-white.svg" />
+        </a>
+      </div>
       <div className="bg-linear-to-r from-transparent mb-4 via-neutral-700 to-transparent h-px w-full" />
       <div>© {new Date().getFullYear()} Savas. Alle Rechte vorbehalten.</div>
       <div>
