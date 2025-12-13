931060a89b402d7b19d3d9f048ca9257e4c87e2a
Ömer <savas.qrx@gmail.com>
2025-12-13 14:20:45 +0100

    change classname

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -14,7 +14,7 @@ export default function About() {
         <div className="flex items-end grid-default-color grid-1">
           <img
             src="assets/coding-pov.png"
-            className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
+            className="absolute scale-[1.75] -right-20 -top-4 md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
           />
           <div className="z-10">
             <p className="headtext">Hi, ich bin Savas</p>
